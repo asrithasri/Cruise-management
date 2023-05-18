3b32049c507055d18485aa606316cb724bc93e0e
asrithasri <asrithk133@gmail.com>
2023-05-18 16:25:23 +0530

    first commit

---
+++ cruise-web/src/main/java/com/cruise/management/CruiseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cruise.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruiseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruiseManagementApplication.class, args);
+	}
+
+}
---
+++ cruise-web/src/main/java/com/cruise/management/InventoryController.java
@@ -0,0 +1,5 @@
+package com.cruise.management;
+
+public class InventoryController {
+
+}
