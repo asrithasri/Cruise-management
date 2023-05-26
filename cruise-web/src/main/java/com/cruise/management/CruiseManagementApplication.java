4a1eb4fdf47d90f223c81d8b91280a3065449ca4
asrithasri <asrithk133@gmail.com>
2023-05-26 14:47:48 +0530

    26-05-2023

--- cruise-web/src/main/java/com/cruise/management/CruiseManagementApplication.java
+++ cruise-web/src/main/java/com/cruise/management/CruiseManagementApplication.java
@@ -2,8 +2,15 @@ package com.cruise.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+
+//@ComponentScan(basePackages= {"com.cruise.management"})
+//@EntityScan(basePackages= {"com.cruise.management"})
+//@EnableJpaRepositories(basePackages= {"com.cruise.management"})
 public class CruiseManagementApplication {
 
 	public static void main(String[] args) {
--- cruise-web/src/main/java/com/cruise/management/InventoryController.java
+++ cruise-web/src/main/java/com/cruise/management/InventoryController.java
@@ -7,12 +7,15 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inventory.entity.Cruise;
+import com.inventory.entity.Inventory;
 import com.inventory.repositoty.CruiseRepository;
 import com.inventory.service.CruiseService;
 import com.inventory.service.InventoryService;
@@ -49,12 +52,17 @@ public class InventoryController {
 	}
 	
 	@GetMapping("/cruise/{id}")
-	public Cruise findCruiseByCruiseId(@PathVariable Long id) {
+	public Cruise findCruiseByCruiseId(@PathVariable Long cruiseId) {
 		return cruiseService.findCruiseById(cruiseId);
 		
 	}
+	@PostMapping("/cruise")
+	 public Cruise createCruise(@RequestBody Cruise cruise) {
+        return cruiseService.createCruise(cruise);
+    }
+	
 	@DeleteMapping("/cruise/{id}")
-	public void deleteCruise(@PathVariable Long id) {
+	public void deleteCruise(@PathVariable Long cruiseId) {
 		cruiseService.deleteCruise(cruiseId);
 		
 	}
@@ -63,6 +71,44 @@ public class InventoryController {
 		return cruiseService.createCruise(cruise);
 		
 	}
+	
+	@GetMapping("/cruises/search")
+    public List<Cruise> searchCruise(@RequestParam String keyword) {
+        return cruiseService.searchCruise(keyword);
+    }
+	@PostMapping("/cruises/save")
+    public Cruise saveCruise(@RequestBody Cruise cruise) {
+        return cruiseService.save(cruise);
+    }
+	
+	
+	
+	
+	 @GetMapping("/inventory")
+	    public List<Inventory> getAllInventories() {
+	        return inventoryService.findAllInventory();
+	    }
+
+	    @GetMapping("/inventory/{id}")
+	    public Inventory getInventoryById(@PathVariable Long id) {
+	        return inventoryService.findInventoryById(id);
+	    }
+
+	    @PostMapping("/inventory")
+	    public Inventory createInventory(@RequestBody Inventory inventory) {
+	        return inventoryService.createInventory(inventory);
+	    }
+
+	    @PutMapping("/inventory")
+	    public Inventory updateInventory(@RequestBody Inventory inventory) {
+	        return inventoryService.updateInventory(inventory);
+	    }
+
+	    @DeleteMapping("/inventory/{id}")
+	    public void deleteInventory(@PathVariable Long invenroryId) {
+	        inventoryService.deleteInventory(invenroryId);
+	    }
+	
 	
 	
 
